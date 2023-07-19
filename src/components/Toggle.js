79b37224f08b4987222a6a8a34df7412b602f073
mary446 <135317707+mary446@users.noreply.github.com>
2023-07-19 21:07:45 +0300

    initial commit

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+
+ export function Toggle() {
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "pink" : "yellow";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
-export default Toggle;
+// function Toggle() {
+//   const [isOn, setIsOn] = useState(false);
+//   console.log({ isOn, setIsOn })
+
+//   function handleClick() {
+//     setIsOn(!isOn)
+//   }
+
+//   return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+// }
